fc6ec5fbe7e06b5ebc35036690cc53ca1c544fd5
0xInNox <111129600+amora92@users.noreply.github.com>
2024-06-29 03:42:33 +0100

    nav
    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,9 +19,7 @@ const Navbar = () => {
           <Link href='/'>
             <Image src='/pineapple.svg' alt='logo' width={74} height={29} />
           </Link>
-          <p className='ml-2 text-lime-400 font-semibold text-lg'>
-            mybartenders.co.uk
-          </p>
+          <p className='ml-2 text-grey-800 font-semibold'>mybartenders.co.uk</p>
         </div>
 
         <div className='hidden lg:flex gap-12'>
