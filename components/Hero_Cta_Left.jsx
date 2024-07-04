fec7faa287f403ce7e2de5ec799b44d272286863
0xInNox <111129600+amora92@users.noreply.github.com>
2024-07-04 01:50:03 +0100

    bottom_logo_removal
    bottom_logo_removal

--- components/Hero_Cta_Left.jsx
+++ components/Hero_Cta_Left.jsx
@@ -58,7 +58,7 @@ const Hero_Cta_Left = () => {
             Cocktail bartender hire, mobile bar, weddings, parties,
             masterclasses:
           </h1>
-          <div className='mt-2 text-4xl font-bold tracking-tight text-lime-400 sm:text-6xl'>
+          <div className='mt-2 text-4xl font-bold text-lime-400 sm:text-6xl'>
             Free Quote
           </div>
           <h2 className='mt-4 text-lg font-semibold leading-8 text-gray-600'>
