b167e9dd329cab46bbff5cac484aa307d563dcde
0xInNox <111129600+amora92@users.noreply.github.com>
2024-07-06 12:05:36 +0100

    SEO changes, mostly pictures
    SEO changes, mostly pictures

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,6 +1,7 @@
-import { Inter } from 'next/font/google'
+import React from 'react'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Inter } from 'next/font/google'
 import '../app/globals.css'
 
 // Import the Inter font
@@ -14,177 +15,103 @@ const Gallery = () => {
         <section className='container mx-auto px-6 lg:px-20 py-12'>
           <h1 className='text-4xl font-bold mb-8 text-center'>Gallery</h1>
 
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6'>
             {/* Gallery Item 1 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/IMG-20240224-WA0027.jpg'
-                alt='Gallery Image 1'
+                src='/IMG-20240224-WA0027.webp'
+                alt='Parisian Absinthe Fountain'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
 
             {/* Gallery Item 2 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/IMG-20240224-WA0048.jpg'
-                alt='Gallery Image 2'
+                src='/IMG-20240224-WA0048.webp'
+                alt='White Russian Cocktail Layered'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
 
             {/* Gallery Item 3 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/IMG-20240224-WA0053.jpg'
-                alt='Gallery Image 3'
+                src='/IMG-20240224-WA0053.webp'
+                alt='White Russian Cocktail Layered Cream'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
 
             {/* Gallery Item 4 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/IMG-20240224-WA0056.jpg'
-                alt='Gallery Image 4'
+                src='/IMG-20240224-WA0056.webp'
+                alt='White Russian Cocktail Layered Cream Cinnamon'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
 
             {/* Gallery Item 5 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/IMG_20220323_122115_003.jpg'
-                alt='Gallery Image 5'
+                src='/IMG_20220323_122115_003.webp'
+                alt='Parisian Absinthe Fountain And Premium Vodka'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
 
             {/* Gallery Item 6 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/fire.jpg'
-                alt='Gallery Image 6'
+                src='/fire.webp'
+                alt='Firebreathing bartenders'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
+
+            {/* Gallery Item 7 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/20220528_173621.jpg'
-                alt='Gallery Image 6'
+                src='/20220528_173621.webp'
+                alt='Garden Cocktail Party SuperCar Fest 2022'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
+
+            {/* Gallery Item 8 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/20220528_183404.jpg'
-                alt='Gallery Image 6'
+                src='/20220528_183404.webp'
+                alt='Garden Cocktail Party SuperCar Fest 2022 Drivers Lounge Menu'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
+
+            {/* Gallery Item 9 */}
             <div className='relative overflow-hidden rounded-lg shadow-md'>
               <img
-                src='/20220528_173609.jpg'
-                alt='Gallery Image 6'
+                src='/20220528_173609.webp'
+                alt='Garden Cocktail Party SuperCar Fest 2022 Service'
                 className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20220510_184836.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20190720_012709.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20210612_175631.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20150428_230641.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/IMG-20240224-WA0005.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20160126_210840.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20160604_194711.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/IMG-20240224-WA0054.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/IMG-20240224-WA0052.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/IMG_20210626_130609_709.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/daiq.png'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20220526_194220.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/20210525_215905.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
-              />
-            </div>
-            <div className='relative overflow-hidden rounded-lg shadow-md'>
-              <img
-                src='/SmartSelect_20190720-013659_Facebook.jpg'
-                alt='Gallery Image 6'
-                className='w-full h-full object-cover transform transition duration-300 hover:scale-105'
+                width={400}
+                height={300}
               />
             </div>
           </div>
