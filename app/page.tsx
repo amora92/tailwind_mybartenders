ed9a4319765925fd1fbc3977af3cc9aec10eae7b
0xInNox <111129600+amora92@users.noreply.github.com>
2024-07-08 01:17:12 +0100

    video_section_commit
    video_section_commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Camp from '@/components/Camp'
 import Hero from '@/components/Hero'
+import VideoSection from '@/components/VideoSection'
 export default function Home () {
   return (
     <>
+      <VideoSection />
       <Hero />
       <Camp />
     </>
