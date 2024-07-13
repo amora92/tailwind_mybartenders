b6456a96113ef80f6b8ebc967680f53ac2681760
0xInNox <111129600+amora92@users.noreply.github.com>
2024-07-13 02:27:20 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout ({
     <html lang='en'>
       <head>
         <link
-          rel='preload'
+          rel='stylesheet'
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
-          as='style'
-          onLoad="this.onload=null;this.rel='stylesheet'"
+          media='print'
+          onLoad="this.media='all'; this.onload=null;"
         />
         <noscript>
           <link
