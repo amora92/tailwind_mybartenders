7566e4bc02bdc37b294fb44a003f14bdeb669cda
0xInNox <111129600+amora92@users.noreply.github.com>
2024-07-16 16:48:42 +0100

    SEO changes
    SEO changes

--- components/Cta_Features.tsx
+++ components/Cta_Features.tsx
@@ -7,20 +7,19 @@ const Cta_Features = () => {
         <div className='mb-10 lg:mb-16 flex justify-center items-center flex-col gap-x-0 gap-y-6 lg:gap-y-0 lg:flex-row lg:justify-between max-md:max-w-lg max-md:mx-auto'>
           <div className='relative w-full text-center lg:text-left lg:w-2/4'>
             <h2 className='text-4xl font-bold text-gray-900 leading-[3.25rem] lg:mb-6 mx-auto max-w-max lg:max-w-md lg:mx-0'>
-              Enjoy an effortless experience, overseen by experts of the drinks
-              industry.
+              Our Portable Cocktail Bar Will Create A Party Anywhere You Wish!
             </h2>
           </div>
           <div className='relative w-full text-center lg:text-left lg:w-2/4'>
             <p className='text-lg font-normal text-gray-500 mb-5'>
-              We will consult you based on our experience, but ultimately abide
-              to your requirements
+              We cater to all needs, from small - private gatherings, to big
+              corporate or family do's.
             </p>
             <a
-              href='https://www.mybartenders.co.uk/services'
+              href='https://www.mybartenders.co.uk/gallery'
               className='flex flex-row items-center justify-center gap-2 text-base font-semibold text-green-400 lg:justify-start hover:text-green-500'
             >
-              Ideas for your party!{' '}
+              Check out our gallery for inspiration.{' '}
               <img src='/idea.svg' alt='Idea Icon' width='30' height='30' />
             </a>
           </div>
@@ -39,8 +38,9 @@ const Cta_Features = () => {
               Here to help!
             </h3>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              Experienced, professional team which can help you with any
-              enquiry.
+              Weddings, Hen or Stag Do's, Private Parties, Mixology
+              Masterclasses, we've done it all, ask and enquire about anything
+              and we will get back to you.
             </p>
           </div>
           <div className='group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
@@ -51,8 +51,8 @@ const Cta_Features = () => {
               We take pride in our service
             </h3>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              We are award winning industry experts, with decades of combined
-              experience.
+              We compete within the drinks industry and have been winning awards
+              since 2012!
             </p>
           </div>
           <div className='group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
@@ -63,8 +63,8 @@ const Cta_Features = () => {
               Responsibly sourced, fresh ingredients{' '}
             </h3>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              We have access to a wide range of suppliers with a long history of
-              working with them.
+              We use the best ingredients we can get our hands on & have long
+              relationships across all suppliers.
             </p>
           </div>
           <div className='group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
@@ -80,8 +80,8 @@ const Cta_Features = () => {
               Timeless cocktails
             </h3>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              Our selection of drinks and service packages makes any occasion
-              special. We've done it all.
+              From 1920s Prohibition Era Cocktails up to Modern Mixology,
+              spanning across 1000s of in-house recipes.
             </p>
           </div>
         </div>
